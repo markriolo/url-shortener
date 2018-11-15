49cc750a1c86cef5dfcf3ad1de00c64fa03ae591
markriolo <mark.riolo.uk@gmail.com>
2018-11-15 16:24:04 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="app">
+    <ui-header></ui-header>
+    <ui-widget></ui-widget>
+  </div>
+</template>
+
+<script>
+  import Header from './components/Header';
+  import Widget from './components/Widget';
+
+  export default {
+    data () {
+      return {}
+    },
+    components: {
+      'ui-header': Header,
+      'ui-widget': Widget
+    }
+  }
+</script>
+
+<style>
+  *{
+    box-sizing: border-box;
+  }
+  body{
+    margin: 0;
+    width: 100%;
+  }
+  #app {
+    font-family: 'Source Sans Pro', sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #555555;
+    font-size: 14pt;
+  }
+
+  h1 {
+    font-size: 22pt;
+    font-weight: bold;
+  }
+
+  a {
+    font-size: 18pt;
+    font-weight: normal;
+    text-decoration: underline;
+    color: #2979bc;
+  }
+
+</style>
